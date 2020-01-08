6686b978c93819b6cd2a5d15eb1b23f8ada7dd1e
Sanhaphat Chaiyara <sanhaphat.binary@gmail.com>
2020-01-08 23:48:05 +0700

    close #14 - add entity FinancialInfo.java

--- FinancialInfo.java
+++ FinancialInfo.java
@@ -1,4 +1,4 @@
-package com.cpe.se62.team08;
+package com.cpe.server.entity;
 
 import lombok.*;
 import javax.persistence.*;
@@ -18,5 +18,23 @@ public class FinancialInfo {
     @Column(name="FinancialInfo_ID")
 
     private @NonNull Long finanId;
+    private @NonNull Long academicy;
+    private @NonNull Long houseincome;
+    private @NonNull String notenough;
 
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = LevelofUse.class)
+    @JoinColumn(name = "LevelofUse_ID", insertable = true)
+    private LevelofUse levelofuse;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = MoneyFormParent.class)
+    @JoinColumn(name = "MoneyFormParent_ID", insertable = true)
+    private MoneyFormParent moneyformparent;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = SpendMoney.class)
+    @JoinColumn(name = "SpendMoney_ID", insertable = true)
+    private SpendMoney spendmoney;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = StudentProfile.class)
+    @JoinColumn(name = "StudentProfile_ID", insertable = true)
+    private StudentProfile studentprofile;
 }
