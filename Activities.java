708b941f3e7ac131b68413dfd0acdb24dd73c6c8
Wannakran Sumneang <junewannakran.2540@gmail.com>
2020-01-08 14:14:38 +0700

    close #16 -Entity Activities

--- Activities.java
+++ Activities.java
@@ -3,7 +3,6 @@ package com.example.server.activities.entity;
 import lombok.*;
 import javax.persistence.*;
 import java.util.Date;
-
 @Getter
 @Setter
 @Entity
@@ -21,8 +20,18 @@ public class Activities {
     private @NonNull Long actid;
     private @NonNull Date date;
     private @NonNull String actname;
-    private @NonNull int year;
-    private @NonNull String institution;
     private @NonNull String tel;
-    
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = SchoolYear.class)
+    @JoinColumn(name = "SchoolYear_ID", insertable = true)
+    private SchoolYear schoolyear;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Institution.class)
+    @JoinColumn(name = "Institution_ID", insertable = true)
+    private Institution institution;
+
+    @ManyToOne(fetch = FetchType.EAGER )
+    @JoinColumn(name="StudentProfile_ID")
+    private StudentProfile studentprofile;
+
 }
