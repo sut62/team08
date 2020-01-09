5d2fa578f18a1cda51729116904c40c9b1cb17db
knpia <knpiasai@gmail.com>
2020-01-10 03:51:32 +0700

    close #22 - ทำ Controller ระบบบันทึกข้อมูลงานวินัยนักศึกษา

---
+++ server/src/main/java/com/example/server/Discipline/controller/PunishController.java
@@ -0,0 +1,29 @@
+package com.example.server.Discipline.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.example.server.Discipline.entity.Punish;
+import com.example.server.Discipline.repository.PunishRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class PunishController {
+
+    @Autowired
+    private PunishRepository punishRepository;
+
+    public PunishController(PunishRepository punishRepository) {
+        this.punishRepository = punishRepository;
+    }
+    @GetMapping("/punishs")
+    public Collection<Punish> Punishs() {
+        return punishRepository.findAll().stream().collect(Collectors.toList());
+    }
+}
