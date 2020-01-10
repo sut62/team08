ab90cc9916b9f420b84335527c38115fe713115f
aifnatcha <amiofok@hotmail.com>
2020-01-10 17:31:49 +0700

    add client

---
+++ client/src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router';
+import Login from '../components/Login';
+import Home from '../components/Home';
+import StudentProfile from "../components/Studentprofile";
+import Activities from "../components/Activities";
+import Discipline from "../components/Discipline";
+import Financial from "../components/Financial";
+import StudentHealthRecord from "../components/StudentHealthRecord"
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+
+        path: '/',
+        name: 'login',
+        component: Login
+    },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/studentprofile',
+            name: 'studentprofile',
+            component: StudentProfile
+        },
+        {
+            path: '/activities',
+            name: 'activities',
+            component: Activities
+        },
+        {
+            path: '/discipline',
+            name: 'discipline',
+            component: Discipline
+        },
+        {
+            path: '/financial',
+            name: 'financial',
+            component: Financial
+        },
+        {
+            path: '/studenthealthrecord',
+            name: 'studenthealthrecord',
+            component: StudentHealthRecord
+        },
+    ]
+});
