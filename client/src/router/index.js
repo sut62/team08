32f4467f64de9494f559f83053666d4ef5cffb43
aifnatcha <amiofok@hotmail.com>
2020-01-10 19:35:39 +0700

    close #40 -edit index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,52 +1,59 @@
-import Vue from 'vue'
-import Router from 'vue-router';
-import Login from '../components/Login';
-import Home from '../components/Home';
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/Login";
+import Home from "../components/Home";
 import StudentProfile from "../components/Studentprofile";
 import Activities from "../components/Activities";
 import Discipline from "../components/Discipline";
 import Financial from "../components/Financial";
-import StudentHealthRecord from "../components/StudentHealthRecord"
+import StudentHealthRecord from "../components/StudentHealthRecord";
+import Scholarship from "../components/Scholarship";
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [{
-
-        path: '/',
-        name: 'login',
-        component: Login
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: Login
     },
-        {
-            path: '/home',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/studentprofile',
-            name: 'studentprofile',
-            component: StudentProfile
-        },
-        {
-            path: '/activities',
-            name: 'activities',
-            component: Activities
-        },
-        {
-            path: '/discipline',
-            name: 'discipline',
-            component: Discipline
-        },
-        {
-            path: '/financial',
-            name: 'financial',
-            component: Financial
-        },
-        {
-            path: '/studenthealthrecord',
-            name: 'studenthealthrecord',
-            component: StudentHealthRecord
-        },
-    ]
+    {
+      path: "/home",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/studentprofile",
+      name: "studentprofile",
+      component: StudentProfile
+    },
+    {
+      path: "/activities",
+      name: "activities",
+      component: Activities
+    },
+    {
+      path: "/discipline",
+      name: "discipline",
+      component: Discipline
+    },
+    {
+      path: "/financial",
+      name: "financial",
+      component: Financial
+    },
+    {
+      path: "/studenthealthrecord",
+      name: "studenthealthrecord",
+      component: StudentHealthRecord
+    },
+
+    {
+      path: "/scholarship",
+      name: "scholarship",
+      component: Scholarship
+    }
+  ]
 });
