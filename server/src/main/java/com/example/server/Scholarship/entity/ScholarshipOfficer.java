53b0dfbc54dd45b797a1316eb0f24333b6e72d3f
Worrakarn Pinchai <pworrakarn@gmail.com>
2020-01-10 16:30:36 +0700

    close #17 - ทำ Entity ของ Scholarship

---
+++ server/src/main/java/com/example/server/Scholarship/entity/ScholarshipOfficer.java
@@ -0,0 +1,37 @@
+package com.example.server.Scholarship.entity;
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name = "ScholarshipOfficer")
+public class ScholarshipOfficer {
+    @Id
+    @SequenceGenerator(name = "ScholarshipOfficer_seq", sequenceName = "ScholarshipOfficer_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ScholarshipOfficer_seq")
+    @Column(name = "ScholarshipOfficer_ID")
+    private @NonNull Long officerid;
+    private @NonNull String officername;
+
+    public Long getOfficerid() {
+        return this.officerid;
+    }
+
+    public void setOfficerid(Long officerid) {
+        this.officerid = officerid;
+    }
+
+    public String getOfficername() {
+        return this.officername;
+    }
+
+    public void setOfficername(String officername) {
+        this.officername = officername;
+    }
+
+}
