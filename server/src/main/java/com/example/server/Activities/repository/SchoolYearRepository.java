6908db5438617d14448e63f09507712b7e9b44b7
Wannakran Sumneang <junewannakran.2540@gmail.com>
2020-01-10 15:19:11 +0700

    close #41 -Repository Activities

---
+++ server/src/main/java/com/example/server/Activities/repository/SchoolYearRepository.java
@@ -0,0 +1,12 @@
+package com.example.server.activities.repository;
+
+import com.example.server.activities.entity.SchoolYear;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:8080")
+public interface SchoolYearRepository extends JpaRepository <SchoolYear, Long> {
+    SchoolYear findById(long yearid);
+}
