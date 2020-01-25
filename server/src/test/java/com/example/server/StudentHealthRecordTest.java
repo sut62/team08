680e2c0326b276b972d3e819dd7918b887fe7845
momaynareerat <amtaisongm@gmail.com>
2020-01-25 22:21:54 +0700

    close #94 -add test StudentHealthRecordTest

---
+++ server/src/test/java/com/example/server/StudentHealthRecordTest.java
@@ -0,0 +1,162 @@
+package com.example.server;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import com.example.server.StudentHealthRecord.entity.*;
+import com.example.server.StudentHealthRecord.repository.*;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+public class StudentHealthRecordTest {
+
+    private Validator validator;
+
+    @Autowired
+    StudentHealthRecordRepository StudentHealthRecordRepository;
+
+    @BeforeEach
+    public void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test 
+    void testCreateStudentHealthRecordOK() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(1L);
+        studentHealthRecord.setHeight(180);
+        studentHealthRecord.setWeight(60);
+        studentHealthRecord = StudentHealthRecordRepository.saveAndFlush(studentHealthRecord);
+
+        Optional<StudentHealthRecord> Result = StudentHealthRecordRepository.findById(studentHealthRecord.getStudentHealthRecordid());
+        assertEquals(1L, Result.get().getStudentHealthRecordid());
+        assertEquals(180, Result.get().getHeight());
+        assertEquals(60, Result.get().getWeight());
+    }
+
+    @Test 
+    void testStudentHealthRecordIdMustNotBeNull() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(null);
+        studentHealthRecord.setHeight(180);
+        studentHealthRecord.setWeight(60);
+
+        Set<ConstraintViolation<StudentHealthRecord>> result = validator.validate(studentHealthRecord);
+
+        assertEquals(1, result.size());
+
+        ConstraintViolation<StudentHealthRecord> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("StudentHealthRecordid", v.getPropertyPath().toString());
+    }
+
+
+    @Test 
+    void testHeightMustNotBeNull() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(1L);
+        studentHealthRecord.setHeight(null);
+        studentHealthRecord.setWeight(60);
+
+        Set<ConstraintViolation<StudentHealthRecord>> result = validator.validate(studentHealthRecord);
+
+        assertEquals(1, result.size());
+
+        ConstraintViolation<StudentHealthRecord> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("height", v.getPropertyPath().toString());
+    }
+
+    @Test 
+    void testWidthMustNotBeNull() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(1L);
+        studentHealthRecord.setHeight(180);
+        studentHealthRecord.setWeight(null);
+
+        Set<ConstraintViolation<StudentHealthRecord>> result = validator.validate(studentHealthRecord);
+
+        assertEquals(1, result.size());
+
+        ConstraintViolation<StudentHealthRecord> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("weight", v.getPropertyPath().toString());
+    }
+
+    @Test 
+    void testHeightMustNotLessThan100() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(1L);
+        studentHealthRecord.setHeight(99);
+        studentHealthRecord.setWeight(60);
+
+        Set<ConstraintViolation<StudentHealthRecord>> result = validator.validate(studentHealthRecord);
+
+        assertEquals(1, result.size());
+
+        ConstraintViolation<StudentHealthRecord> v = result.iterator().next();
+        assertEquals("must be greater than or equal to 100", v.getMessage());
+        assertEquals("height", v.getPropertyPath().toString());
+    }
+
+    @Test 
+    void testHeightMustNotMoreThan220() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(1L);
+        studentHealthRecord.setHeight(221);
+        studentHealthRecord.setWeight(60);
+
+        Set<ConstraintViolation<StudentHealthRecord>> result = validator.validate(studentHealthRecord);
+
+        assertEquals(1, result.size());
+
+        ConstraintViolation<StudentHealthRecord> v = result.iterator().next();
+        assertEquals("must be less than or equal to 220", v.getMessage());
+        assertEquals("height", v.getPropertyPath().toString());
+    }
+
+    @Test 
+    void testWeightMustNotLessThan30() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(1L);
+        studentHealthRecord.setHeight(180);
+        studentHealthRecord.setWeight(29);
+
+        Set<ConstraintViolation<StudentHealthRecord>> result = validator.validate(studentHealthRecord);
+
+        assertEquals(1, result.size());
+
+        ConstraintViolation<StudentHealthRecord> v = result.iterator().next();
+        assertEquals("must be greater than or equal to 30", v.getMessage());
+        assertEquals("weight", v.getPropertyPath().toString());
+    }
+
+    @Test 
+    void testWeightMustNotMoreThan200() {
+        StudentHealthRecord studentHealthRecord = new StudentHealthRecord();
+        studentHealthRecord.setStudentHealthRecordid(1L);
+        studentHealthRecord.setHeight(180);
+        studentHealthRecord.setWeight(201);
+
+        Set<ConstraintViolation<StudentHealthRecord>> result = validator.validate(studentHealthRecord);
+
+        assertEquals(1, result.size());
+
+        ConstraintViolation<StudentHealthRecord> v = result.iterator().next();
+        assertEquals("must be less than or equal to 200", v.getMessage());
+        assertEquals("weight", v.getPropertyPath().toString());
+    }
+}
