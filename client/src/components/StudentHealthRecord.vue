8b09e50d0d42673c15b2c919ac86b2f505c69c84
momaynareerat <amtaisongm@gmail.com>
2020-01-26 17:47:26 +0700

    close #114 -edit UI StudentHealthRecord

--- client/src/components/StudentHealthRecord.vue
+++ client/src/components/StudentHealthRecord.vue
@@ -26,11 +26,28 @@
       </v-layout>
     </div>
 
-    <v-row justify="center">
-      <v-col cols="7">
-        <v-form v-model="valid" ref="form">
+   <v-row justify="center">
+        <v-col cols="5">
           <v-row justify="center">
-            <v-col cols="5">
+            <v-col cols="3">
+              <div
+                v-if="saveStatus.isSuccess"
+                style="border: 1px solid #79FFBA; border-radius: 5px; background-color: #B2FFD7; align-items: center"
+              >
+                <div style="padding: 15px; color: #029E4E">{{saveStatus.message}}</div>
+              </div>
+              <div
+                v-if="saveStatus.isFail"
+                style="border: 1px solid #FFA879; border-radius: 5px; background-color: #FFD6B2; align-items: center"
+              >
+                <div style="padding: 15px; color: #733600">{{saveStatus.message}}</div>
+              </div>
+            </v-col>
+          </v-row>
+          <v-form v-model="valid" ref="form">
+            <v-row justify="center">
+              <v-col cols="6">
+
               <v-select
                 label="กรุณาเลือกนักศึกษา"
                 outlined
@@ -103,7 +120,7 @@
 import http from "../http-common"
 
 export default {
-  name: "prescriptioStudentHealthRecordn",
+  name: "StudentHealthRecord",
   data() {
     return {
       studenthealthRecord: {
@@ -115,10 +132,15 @@ export default {
         active: false
       },
       submitted: false,
-      valid: false,
       student: [],
       drugallergyhistory: [],
-      congenitalDisease: []
+      congenitalDisease: [],
+       valid: false,
+       saveStatus: {
+        isSuccess: false,
+        isFail: false,
+        message: ""
+      }
     }
   },
   methods: {
@@ -172,19 +194,31 @@ export default {
             this.studenthealthRecord.drugallergyhistorysId,
             this.studenthealthRecord
         )
-        .then(response => {
-          console.log(response.data)
-          if (response.data) {
-            alert("บันทึกข้อมูลสำเร็จ")
+         .then(response => {
+          if (response) {
+            this.saveStatus.message = "บันทึกข้อมูลสำเร็จ"
+            this.saveStatus.isSuccess = true
+            setTimeout(() => {
+              this.saveStatus.message = ""
+              this.saveStatus.isSuccess = false
+            }, 3000)
           } else {
-            alert("กรุณากรอกข้อมูลให้ถูกต้อง")
+            this.saveStatus.message = "บันทึกข้อมูลไม่สำเร็จ"
+            this.saveStatus.isFail = true
+            setTimeout(() => {
+              this.saveStatus.message = ""
+              this.saveStatus.isFail = false
+            }, 3000)
           }
         })
-        .catch(e => {
-          alert("กรุณากรอกข้อมูลให้ถูกต้อง")
-          console.log(e)
+        .catch(() => {
+          this.saveStatus.message = "บันทึกข้อมูลไม่สำเร็จ"
+          this.saveStatus.isFail = true
+           setTimeout(() => {
+              this.saveStatus.message = ""
+              this.saveStatus.isFail = false
+            }, 3000)
         })
-      this.submitted = true
     },
     clear() {
       this.$refs.form.reset()
