1cf0634b391eec8eaff7fb23f06e6a4525319dff
Wannakran Sumneang <junewannakran.2540@gmail.com>
2020-02-06 23:38:03 +0700

    close #145 - edit repository Activities

--- server/src/main/java/com/example/server/Activities/repository/ActivitiesRepository.java
+++ server/src/main/java/com/example/server/Activities/repository/ActivitiesRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:8081")
 public interface ActivitiesRepository extends JpaRepository <Activities, Long> {
     Activities findById(long actid);
 
